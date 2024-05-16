f13e2c258664869cb91f41bb116dcd2ed34cabda
Jessica Markoski <130881269+s4007154@users.noreply.github.com>
2024-05-16 20:21:07 +1000

    Update SearchForum.java changes

--- SearchForum.java
+++ SearchForum.java
@@ -10,74 +10,18 @@ enum Filter {
 }
 
 //mock database which is storing all Posts on the Code QA platform
-class Database {
-    //ArrayList of all stored Posts
-    static ArrayList<Post> Posts = new ArrayList<>();
-
-}
-
-//User Class
-class User {
-
-    String username;
-    String password;
-    ArrayList<Post> UserPosts = new ArrayList<>();
-
-}
-
-//Post Class
-class Post {
-    int postID;
-    User author;
-    String content;
-    int upvote;
-    int downvote;
-    boolean flagged = false;
-    // date of creation post date initalised as LocalDate postDate = LocallDate.of(YYYY, MM, DD)
-    LocalDate postDate;
-
-    //creates a post object
-    Post (int postID, User user, String content, LocalDate postDate){
-        this.postID = postID;
-        this.author = user;
-        this.content = content;
-        this.postDate = postDate;
-        //stores created Post in Database
-        Database.Posts.add(this);
-    }
-
-    void deletePost(){
-        
-        content = "Content has been removed";
-        author = null;
-
-        //updates content and author in the Database
-        for (Post post : Database.Posts){
-            if (this.postID == post.postID){
-                post.content = this.content;
-                post.author = this.author;
-                post.postDate = this.postDate;
-            }
+public static class Database {
+    public boolean postsFound(ArrayList<Post> findPostInSearchForum) {
+        if (findPostInSearchForum != null) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
-
-    void flagPost(){
-        flagged = true;
-
-        //updates flagged in the database
-        for (Post post : Database.Posts){
-            if (this.postID == post.postID){
-                post.flagged = this.flagged;
-            }
-        }
-    }
-
-}
-
-class SearchForum {
-
+    
     //returns a List of Posts that match a search query
-    static ArrayList<Post> findPostInSearchForum(String query, Filter selectedFilter){
+    public static ArrayList<Post> findPostInSearchForum(String query, Filter selectedFilter){
         
         //creates empty list for results
         ArrayList<Post> searchResults = new ArrayList<>();
@@ -103,31 +47,104 @@ class SearchForum {
                 //sort the search results by highest Downvote count to lowest Downvote count
                 //update the searchResults ArrayList
             }
+        }
+        else if (selectedFilter == Filter.OLDEST) {
+            for (Post post : searchResults) {
+                //sort the search results by highest Downvote count to lowest Downvote count
+                //update the searchResults ArrayList
+            }
+        }
 
+        else if (selectedFilter == Filter.NEWEST) {
+            for (Post post : searchResults) {
+                //sort the search results by highest Downvote count to lowest Downvote count
+                //update the searchResults ArrayList
+            }
         }
         
         //return the search results
         return searchResults;
-        
-    } 
+    }
+}
+
+//User Class
+public class User {
+    String username;
+    String password;
+    ArrayList<Post> UserPosts = new ArrayList<>();
 
 }
 
+//Post Class
+public class Post {
+    int postID;
+    User author;
+    String content;
+    int upvote;
+    int downvote;
+    boolean flagged = false;
+    // date of creation post date initalised as LocalDate postDate = LocallDate.of(YYYY, MM, DD)
+    LocalDate postDate;
 
-class WebsiteUI {
-    
-    //user inputs a search keyword/phrase and selects a search Filter
-    void searchInquiry(String userInput, Filter selectedFilter){
+    //creates a post object
+    Post (int postID, User user, String content, LocalDate postDate){
+        this.postID = postID;
+        this.author = user;
+        this.content = content;
+        this.postDate = postDate;
+        //stores created Post in Database
+        Database.Posts.add(this);
+    }
+
+    public void deletePost(){
         
+        content = "Content has been removed";
+        author = null;
+
+        //updates content and author in the Database
+        for (Post post : Database.Posts){
+            if (this.postID == post.postID){
+                post.content = this.content;
+                post.author = this.author;
+                post.postDate = this.postDate;
+            }
+        }
+    }
+
+    public void flagPost(){
+        flagged = true;
+
+        //updates flagged in the database
+        for (Post post : Database.Posts){
+            if (this.postID == post.postID){
+                post.flagged = this.flagged;
+            }
+        }
+    }
+
+}
+
+public static class SearchForum {
+        //ArrayList of all stored Posts
+        public static ArrayList<Post> searchResults = new ArrayList<Post>();
+
         //gets search results from the Search Forum class
         ArrayList<Post> searchResults = SearchForum.findPostInSearchForum(userInput, selectedFilter);
+    
+}
+
+
+public static class WebsiteUI {
+    
+    //user inputs a search keyword/phrase and selects a search Filter
+    public void searchInquiry(String userInput, Filter selectedFilter){
         
         //displays results to the user
-        displayResults(searchResults);
+        displayResults(SearchForum.searchResults);
     }
 
     //takes in a list of Posts
-    void displayResults(ArrayList<Post> searchResults){
+    public void displayResults(ArrayList<Post> searchResults){
         
         System.out.println("Search Results: ");
         //if the list of posts is not null
