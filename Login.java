4fcb0c3e088d4704163f157d0708db533d17be33
Jessica Markoski <130881269+s4007154@users.noreply.github.com>
2024-05-17 16:36:00 +1000

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class User {
-    private boolean loggedIn = false;
     private String username;
     private String password;
     private UserStatus status = UserStatus.NORMAL;
